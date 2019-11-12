b592e00013c8a147493660671c6516277aa84a00
Your Name <you@example.com>
2019-11-12 13:41:09 +0900

    second

--- fizz_buzz_app/views.py
+++ fizz_buzz_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from .forms import NumForm
 # Create your views here.
+
+def numform(request):
+    numform = NumForm()
+    params = {
+        'numform': numform
+    }
+    return render(request, 'result.html', params)
+

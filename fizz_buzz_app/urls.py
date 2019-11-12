003ce6df1480aa54436eba7b36190873404c5c10
Your Name <you@example.com>
2019-11-12 15:35:35 +0900

    数字を表示するだけdjango完成。

--- fizz_buzz_app/urls.py
+++ fizz_buzz_app/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from .  import views
+from . import views
 
 urlpatterns = [
     path('', views.numform, name='form'),
+    path('result/', views.result, name='result')
 ]
--- fizz_buzz_app/views.py
+++ fizz_buzz_app/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
-from .forms import NumForm
+from .forms import NumForm#htmlのinputで事足りるからいらん
 # Create your views here.
 
 def numform(request):
-    numform = NumForm()
+    return render(request, 'num_form.html')
+
+def result(request):
+    print(request.POST)
+    numform = request.POST['number']
     params = {
         'numform': numform
     }
     return render(request, 'result.html', params)
-

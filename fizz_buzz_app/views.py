6424ce11c12705c8c11495154d7238ade5f084b5
Your Name <you@example.com>
2019-11-12 23:32:01 +0900

    完成形

--- fizz_buzz_app/views.py
+++ fizz_buzz_app/views.py
@@ -1,14 +1,35 @@
 from django.shortcuts import render
-from .forms import NumForm#htmlのinputで事足りるからいらん
+from .forms import NumForm #htmlのinputで事足りるからいらん
 # Create your views here.
 
 def numform(request):
     return render(request, 'num_form.html')
 
 def result(request):
-    print(request.POST)
-    numform = request.POST['number']
+    numform = int(request.POST['number'])
+    result_list = []
+    for i in range(1, numform+1):
+        judge = fizz_buzz_judge(i)
+        result_list.append(judge)
     params = {
-        'numform': numform
+        'numform': numform,
+        'result_list': result_list,
     }
     return render(request, 'result.html', params)
+
+
+
+
+def fizz_buzz_judge(x):
+
+    if x % 15 == 0:
+        judge = 'FIZZBUZZ'
+    elif x % 5 == 0:
+        judge = 'Buzz'
+    elif x % 3 == 0:
+        judge = 'Fizz'
+    else:
+        judge = str(x)
+    return judge
+
+
